9e5071f5076a0123566bb6d05598ceaa559afa64
chikondi <chikondiman@gmail.com>
2020-12-22 13:23:39 -0500

    initial commit

---
+++ knowledge.cs
@@ -0,0 +1,29 @@
+using System;
+namespace Knowledge
+{
+
+class Program {
+
+static void Main (string[] args){
+
+Console.Title = "knowledge 0.1"
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Welcome to Knowledge, what's your name?");
+
+Console.Readline();
+
+Console.WriteLine("i am eden");
+
+
+
+}
+}
+
+
+
+
+
+
+}
